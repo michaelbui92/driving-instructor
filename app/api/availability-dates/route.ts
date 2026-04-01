a04b341fedeb896bcd4832cc98ee9962528da918
Ubuntu <ubuntu@ip-172-31-1-236.ap-southeast-2.compute.internal>
2026-04-01 20:43:07 +0000

    Add 12pm back - all hours available by default

--- app/api/availability-dates/route.ts
+++ app/api/availability-dates/route.ts
@@ -79,7 +79,7 @@ export async function GET(request: NextRequest) {
       const isWeekend = dayOfWeek === 0 || dayOfWeek === 6
       
       // ALL hours available by default - rules will block what needs blocking
-      const defaultSlots = ['8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM', '7:00 PM', '8:00 PM']
+      const defaultSlots = ['8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM', '7:00 PM', '8:00 PM']
       
       // Blocked times for this date
       const blockedTimes = new Set<string>((blockedSlots || []).filter(b => b.date === date).map(b => b.time))
