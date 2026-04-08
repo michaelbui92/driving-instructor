d543974076a4c3494900f439bba0583f09d27423
Michael Bui <michaelbui@outlook.com.au>
2026-04-08 12:12:22 +0000

    fix: disable AOS scroll animations on touch devices to prevent jank

--- components/AOSInit.tsx
+++ components/AOSInit.tsx
@@ -5,13 +5,18 @@ import AOS from 'aos'
 
 export default function AOSInit() {
   useEffect(() => {
-    AOS.init({
-      duration: 800,
-      easing: 'ease-out-cubic',
-      once: true,
-      offset: 50,
-      delay: 0,
-    })
+    // Disable AOS on touch devices to prevent scroll jank
+    const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0
+
+    if (!isTouchDevice) {
+      AOS.init({
+        duration: 800,
+        easing: 'ease-out-cubic',
+        once: true,
+        offset: 50,
+        delay: 0,
+      })
+    }
   }, [])
 
   return null
